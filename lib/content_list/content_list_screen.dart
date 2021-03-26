919102ea2e92c9405624d9bfccdac65b555f5663
Fernando Trigoso <ftrigoso@dropsource.com>
2021-03-26 15:26:11 -0400

    tweak

--- lib/content_list/content_list_screen.dart
+++ lib/content_list/content_list_screen.dart
@@ -95,15 +95,3 @@ class CategoryCard extends StatelessWidget {
     );
   }
 }
-
-const BoxDecoration shadowDecoration = BoxDecoration(
-  borderRadius: BorderRadius.all(Radius.circular(15)),
-  color: Colors.white,
-  boxShadow: [
-    BoxShadow(
-      color: Color(0x3300001A),
-      blurRadius: 6,
-      offset: Offset(0, 5), // changes position of shadow
-    ),
-  ],
-);
