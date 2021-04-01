599aa60f793dd7d3de6615ee2c96c3af6070753f
Fernando Trigoso <ftrigoso@dropsource.com>
2021-04-01 13:39:45 -0400

    Simplified code, stories and test. Changed some names.

---
+++ stories/obscurable_text_field_stories.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:monarch_samples/text_fields/obscurable_text_field.dart';
+
+Widget obscured_text() =>
+    ObscurableTextfield(
+      value: 'P@ssw0rd!',
+    );
+
+Widget clear_text() =>
+    ObscurableTextfield(
+      value: 'P@ssw0rd!',
+      obscureText: false,
+    );
+
+Widget empty_text() =>
+    ObscurableTextfield();
+
