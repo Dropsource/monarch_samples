beee10949cbfc845042abee7fec47387cff7e790
Fernando Trigoso <ftrigoso@dropsource.com>
2021-09-29 14:41:06 -0400

    Using colorScheme in Monarch theme instead for accentColor

--- lib/themes/custom_theme.dart
+++ lib/themes/custom_theme.dart
@@ -1,28 +1,30 @@
-/// Monarch Themes Documentation: 
+/// Monarch Themes Documentation:
 /// https://monarchapp.io/docs/themes
 
 import 'package:flutter/material.dart';
 import 'package:monarch_annotations/monarch_annotations.dart';
 
-
 @MonarchTheme('Monarch Theme')
 final monarchTheme = ThemeData.dark().copyWith(
-  primaryColor: monarchPrimaryColor,
-  accentColor: monarchAccentColor,
-  scaffoldBackgroundColor: monarchBackgroundColor,
-  textTheme: TextTheme(
-    headline5: TextStyle(
-        color: monarchPrimaryColor,
-        fontSize: 34.0,
-        fontWeight: FontWeight.w600),
-    headline6: TextStyle(
-        color: monarchPrimaryColor, fontSize: 26, fontWeight: FontWeight.w600),
-    bodyText1: TextStyle(
-        color: monarchTextColor, fontSize: 20, fontWeight: FontWeight.w600),
-    bodyText2: TextStyle(
-        color: monarchTextColor, fontSize: 14, fontWeight: FontWeight.w500)
-  )
-);
+    primaryColor: monarchPrimaryColor,
+    colorScheme:
+        ThemeData.dark().colorScheme.copyWith(secondary: monarchAccentColor),
+    scaffoldBackgroundColor: monarchBackgroundColor,
+    textTheme: TextTheme(
+        headline5: TextStyle(
+            color: monarchPrimaryColor,
+            fontSize: 34.0,
+            fontWeight: FontWeight.w600),
+        headline6: TextStyle(
+            color: monarchPrimaryColor,
+            fontSize: 26,
+            fontWeight: FontWeight.w600),
+        bodyText1: TextStyle(
+            color: monarchTextColor, fontSize: 20, fontWeight: FontWeight.w600),
+        bodyText2: TextStyle(
+            color: monarchTextColor,
+            fontSize: 14,
+            fontWeight: FontWeight.w500)));
 
 const monarchPrimaryColor = Color(0xff00b3b8);
 const monarchAccentColor = Color(0xff00c5ca);
