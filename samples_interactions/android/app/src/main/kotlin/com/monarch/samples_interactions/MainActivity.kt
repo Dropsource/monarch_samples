23f5932293c07edb7db320a1cdcfb07301f1f47f
Steve Chikwiri <37661480+mijiga@users.noreply.github.com>
2023-08-21 10:14:33 +0200

    Revert "initial commit"
    This reverts commit ffb9094cbba4cdabc9db8fccf93a405a4ab85ae0.

--- samples_interactions/android/app/src/main/kotlin/com/monarch/samples_interactions/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.monarch.samples_interactions
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- samples_interactions/lib/model/expense_list_data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:samples_interactions/model/expense.dart';
-
-List<Expense> expenses = [
-  Expense(title: "Dinner", date: "May 30", amount: 50),
-  Expense(title: "Lunch", date: "May 29", amount: 30),
-  Expense(title: "Shopping", date: "May 26", amount: 75),
-  Expense(title: "Groceries", date: "May 22", amount: 99),
-];
