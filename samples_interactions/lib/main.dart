ffb2830bc0b45286ff511b41426d90299ba60fad
Steve Chikwiri <37661480+mijiga@users.noreply.github.com>
2023-09-04 08:38:43 +0200

    UI setup

---
+++ samples_interactions/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:samples_interactions/expense_list_screen.dart';
+import 'package:samples_interactions/model/expense_list_data.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ExpenseListScreen(expenses: expenses),
+    );
+  }
+}
---
+++ samples_interactions/lib/model/expense.dart
@@ -0,0 +1,7 @@
+class Expense {
+  final String title;
+  final String date;
+  final int amount;
+
+  Expense({required this.title, required this.date, required this.amount});
+}
