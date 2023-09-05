5cb30657b8c7742e2380acf685ba90e248b9e96b
Steve Chikwiri <37661480+mijiga@users.noreply.github.com>
2023-09-05 17:18:25 +0200

    reformat story names

--- samples_patterns/stories/page_transitions_stories.dart
+++ samples_patterns/stories/page_transitions_stories.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:monarch_samples_patterns/page_transitions/expense_list_widget.dart';
 import 'package:monarch_samples_patterns/page_transitions/model/expense_list_data.dart';
 
-Widget noTransition() => ExpenseListWidget(
+Widget no_transition() => ExpenseListWidget(
     expenses: expenses,
     transitionBuilder: (context, animation, secondaryAnimation, child) {
       return child;
     });
 
-Widget bottomUpTransition() => ExpenseListWidget(
+Widget bottom_up_transition() => ExpenseListWidget(
       expenses: expenses,
       transitionBuilder: (context, animation, secondaryAnimation, child) {
         final tween = Tween(begin: const Offset(0.0, 1.0), end: Offset.zero);
@@ -19,7 +19,7 @@ Widget bottomUpTransition() => ExpenseListWidget(
       },
     );
 
-Widget rightToLeftTransition() => ExpenseListWidget(
+Widget right_to_left_transition() => ExpenseListWidget(
       expenses: expenses,
       transitionBuilder: (context, animation, secondaryAnimation, child) {
         final tween = Tween(begin: const Offset(1.0, 0.0), end: Offset.zero);
